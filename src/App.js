1f5dbbd6259417a296ceef2244e4d74e88dee8a6
Seckin Ozturk <franseck102@gmail.com>
2024-08-24 22:46:29 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Lesson from "./components/Lesson";
+import Footer from "./components/Footer";
+import "./scss/app.scss"
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Lesson />
+      <Footer/>
+    </>
+
+  );
+}
+
+export default App;
